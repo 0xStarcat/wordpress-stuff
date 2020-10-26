9fc0f5ddfe887be27948002797458d101a314acb
0xstarcat <xxStarcat@gmail.com>
2020-10-25 21:54:45 -0400

    draft permalink

--- utilities/wp-gatsby-config.php
+++ utilities/wp-gatsby-config.php
@@ -6,25 +6,41 @@
 // include('wp-gatsby-config.php');
 
 
+// gets permalink for draft posts
+// https://wordpress.stackexchange.com/questions/41588/how-to-get-the-clean-permalink-in-a-draft
+function get_draft_permalink( $post_id ) {
+
+  require_once ABSPATH . '/wp-admin/includes/post.php';
+  list( $permalink, $postname ) = get_sample_permalink( $post_id );
+
+  return str_replace( '%pagename%', 'preview', $permalink );
+}
+
 
 //
 //
 // CUSTOMIZE POST PREVIEW LINK
-// Make sure to edit the $preview_domain variable!
+// Make sure to edit the PREVIEW_DOMAIN variable in functions.php!
 // 
 // 
 
 add_filter('preview_post_link', function ($link) {
   global $post;
 	$post_ID = $post->ID;
+	
+	if ($post->post_status != 'publish') {
+         $uri = get_draft_permalink($post_ID);
+    } else {
+		$uri = get_permalink($post -> ID);
+	}
 
 	return PREVIEW_DOMAIN
-		. wp_make_link_relative(get_permalink($post -> ID))
+		. wp_make_link_relative($uri)
 		. '?preview=true&nonce='
 		. wp_create_nonce('wp_rest')
 	    . '&id='
-    . $post_ID
-    . '&post_type='
+		. $post_ID
+		. '&post_type='
 		. get_post_type($post);
 });
 
